318de23e8a3c98a8ef6be5b3cf35b83518e5c730
AhmadImam14 <ahmad0903998@gmail.com>
2026-06-01 19:43:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let cart = {};
+
+// QUANTITY SYSTEM
+function changeQty(name, price, change){
+
+  if(!cart[name]) cart[name] = {price, qty:0};
+
+  cart[name].qty += change;
+
+  if(cart[name].qty < 0) cart[name].qty = 0;
+
+  document.getElementById('qty-'+name).innerText = cart[name].qty;
+
+  updateCart();
+}
+
+// UPDATE CART
+function updateCart(){
+
+  let items = document.getElementById('cart-items');
+  let total = 0;
+  let count = 0;
+
+  items.innerHTML = '';
+
+  for(let key in cart){
+
+    if(cart[key].qty > 0){
+
+      let itemTotal = cart[key].qty * cart[key].price;
+
+      total += itemTotal;
+      count += cart[key].qty;
+
+      items.innerHTML += `<p>${key} x${cart[key].qty} - ₦${itemTotal}</p>`;
+    }
+  }
+
+  document.getElementById('cart-total').innerText = total;
+  document.getElementById('cart-count').innerText = count;
+  document.getElementById('cart-count-2').innerText = count;
+}
+
+// CART TOGGLE
+function toggleCart(){
+  document.getElementById('cart-panel').classList.toggle('active');
+}
+
+// CHECKOUT
+function checkout(){
+
+  let msg = "Hello, I want to order:%0A";
+  let total = 0;
+
+  for(let key in cart){
+    if(cart[key].qty > 0){
+      let itemTotal = cart[key].qty * cart[key].price;
+      msg += `- ${key} x${cart[key].qty} = ₦${itemTotal}%0A`;
+      total += itemTotal;
+    }
+  }
+
+  msg += `%0ATotal: ₦${total}`;
+
+  window.open("https://wa.me/2349126672440?text=" + msg, "_blank");
+}
+
+// DARK MODE
+function toggleDarkMode(){
+  document.body.classList.toggle('dark');
+}
+
+// FADE IN SCROLL
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+document.querySelectorAll('.fade').forEach(el => observer.observe(el));
